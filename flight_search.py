b6932cad20618d40cbfb42f9364dfa0f06515a96
SamroodAli <samroodali@live.com>
2021-01-19 19:11:37 +0530

    Add:get flights prices and data

--- flight_search.py
+++ flight_search.py
@@ -5,20 +5,48 @@ import requests
 class FlightSearch:
     def __init__(self):
         self.api_key = os.environ.get("TEQUILA_API_KEY")
+
+        # location code endpoint
+        self.location_endpoint = "https://tequila-api.kiwi.com/locations/query"
+
+        # flights search api
+        self.flights_search_api_endpoint = "https://tequila-api.kiwi.com/v2/search"
+
+        # Api authentication header data
+        self.headers = {
+            "apikey": self.api_key
+        }
         self.location_parameters = {
-            "apikey": self.api_key,
             "locale": "en-US",
             "location_types": "airport",
             "limit": 1,
             "active_only": "true",
         }
-        self.location_endpoint = "https://tequila-api.kiwi.com/locations/query"
-
-        # flights search api and parameters data
-        self.flights_search_api_endpoint = "https://tequila-api.kiwi.com/v2/search"
 
     def get_iata_code(self, airport_name):
         self.location_parameters["term"] = airport_name
-        response = requests.get(self.location_endpoint, params=self.location_parameters)
+        response = requests.get(self.location_endpoint, params=self.location_parameters, headers=self.headers)
         iata_code = response.json()["locations"][0]["id"]
         return iata_code
+
+    def check_flights(self, origin_city_code, destination_city_code, from_time, to_time, curr):
+        api_parameters = {
+            "fly_from": origin_city_code,
+            "fly_to": destination_city_code,
+            "date_from": from_time.strftime("%d/%m/%Y"),
+            "date_to": to_time.strftime("%d/%m/%Y"),
+            "nights_in_dst_from": 7,
+            "nights_in_dst_to": 28,
+            "flights_type": "round",
+            "curr": curr
+        }
+
+        response = requests.get(
+            url=f"{self.flights_search_api_endpoint}",
+            params=api_parameters,
+            headers = self.headers
+        )
+
+        data = response.json()["data"][0]
+
+        print(data["price"])
--- main.py
+++ main.py
@@ -9,7 +9,7 @@ flight_search = FlightSearch()
 
 sheet_data = data_manager.get_sheets_data()
 
-if sheet_data[0]["iataCode"]=="":
+if sheet_data[0]["iataCode"] == "":
     for row in sheet_data:
         row["iataCode"] = flight_search.get_iata_code(row["city"])
     # Update sheets data
@@ -20,3 +20,16 @@ if sheet_data[0]["iataCode"]=="":
 # Get dates
 tomorrow = datetime.now() + timedelta(days=1)
 six_months_from_now = tomorrow + timedelta(days=6*30)
+
+# Flight constants
+origin_city_code = "CCJ"
+flight_data_currency = "INR"
+
+for destination in sheet_data:
+    flight = flight_search.check_flights(
+        origin_city_code=origin_city_code,
+        destination_city_code=destination["iataCode"],
+        from_time=tomorrow,
+        to_time=six_months_from_now,
+        curr=flight_data_currency
+    )
