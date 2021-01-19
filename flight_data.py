5ece998270cebff98324ded444d7df6b52c5692d
SamroodAli <samroodali@live.com>
2021-01-19 19:16:10 +0530

    Add:Flight data class

---
+++ flight_data.py
@@ -0,0 +1,8 @@
+class FlightData:
+    def __init__(self, price, origin_city, origin_airport, destination_city, destination_airport, travel_data, return_date):
+        self.price = price,
+        self.origin_city = origin_city,
+        self.origin_airport = origin_airport,
+        self.destination_city = destination_city,
+        self.travel_date = travel_data
+        self.return_date = return_date
